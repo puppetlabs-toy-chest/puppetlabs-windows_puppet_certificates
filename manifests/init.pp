89bcda756a1db4785b89ad3d55565115b07fccdc
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-10-25 10:15:40 -0700

    (maint) Update for Puppet 4 compatibility

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,25 @@
-class windows_puppet_certificates {
-  # Add the Puppet Master certificate into the Trusted Root CA
-  windows_puppet_certificates::windows_cert { 'puppet_master_windows_certificate':
-    cert_path => "${::settings::confdir}/ssl/certs/ca.pem",
-    key_path  => '',
-    cert_type => 'trusted_root_ca',
+class windows_puppet_certificates(
+    Enum['present', 'absent'] $ensure = 'present',
+    Boolean $manage_master_cert = true,
+    Boolean $manage_client_cert = false,
+  ) {
+  if $manage_master_cert {
+    # Add the Puppet Master certificate into the Trusted Root CA
+    windows_puppet_certificates::windows_certificate { 'puppet_master_windows_certificate':
+      ensure    => $ensure,
+      cert_path => "${::settings::confdir}/ssl/certs/ca.pem",
+      key_path  => undef,
+      cert_type => 'trusted_root_ca',
+    }
   }
 
-  # Add the client certificate (with private key) to the Personal certificates
-  windows_puppet_certificates::windows_cert { 'puppet_client_windows_certificate':
-    cert_path => "${::settings::confdir}/ssl/certs/${::clientcert}.pem",
-    key_path  => "${::settings::confdir}/ssl/private_keys/${::clientcert}.pem",
-    cert_type => 'personal',
+  if $manage_client_cert {
+    # Add the client certificate (with private key) to the Personal certificates
+    windows_puppet_certificates::windows_certificate { 'puppet_client_windows_certificate':
+      ensure    => $ensure,
+      cert_path => "${::settings::confdir}/ssl/certs/${::clientcert}.pem",
+      key_path  => "${::settings::confdir}/ssl/private_keys/${::clientcert}.pem",
+      cert_type => 'personal',
+    }
   }
 }
--- manifests/windows_certificate.pp
+++ manifests/windows_certificate.pp
@@ -1,11 +1,13 @@
-define windows_puppet_certificates::windows_cert (
-  $cert_path,
-  $key_path,
-  $cert_type,
+define windows_puppet_certificates::windows_certificate (
+  # Currently only 'present' is supported
+  Enum['present'] $ensure = 'present',
+  Stdlib::Windowspath $cert_path,
+  Optional[Stdlib::Windowspath] $key_path,
+  Enum['trusted_root_ca', 'personal'] $cert_type,
 ) {
   exec { $name:
-    command  => template('profile/windows/puppet_certs_command.ps1'),
-    onlyif   => template('profile/windows/puppet_certs_onlyif.ps1'),
+    command  => template('windows_puppet_certificates/puppet_certs_command.ps1'),
+    onlyif   => template('windows_puppet_certificates/puppet_certs_onlyif.ps1'),
     provider => powershell,
   }
 }
