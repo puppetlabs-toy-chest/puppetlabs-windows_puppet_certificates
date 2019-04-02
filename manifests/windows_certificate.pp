39ffa6198a097aeab10e776f568d4674c8b6d44b
Gene Liverman <gene.liverman+06301990@puppet.com>
2019-04-02 09:02:12 -0400

    Updated to PDK 1.9.1
    - Ran `pdk convert` to get this udpated to the current version of the PDK
    - Updated metadata.json to allow Puppet 6
    - Fixed open-ended module requirements per `pdk validate`
    - Fixed parameter ordering per `pdk validate`
    - Added `.sync.yml`

--- manifests/windows_certificate.pp
+++ manifests/windows_certificate.pp
@@ -1,9 +1,9 @@
+# Currently only 'present' is supported
 define windows_puppet_certificates::windows_certificate (
-  # Currently only 'present' is supported
-  Enum['present'] $ensure = 'present',
   Stdlib::Windowspath $cert_path,
-  Optional[Stdlib::Windowspath] $key_path,
   Enum['trusted_root_ca', 'personal'] $cert_type,
+  Enum['present'] $ensure = 'present',
+  Optional[Stdlib::Windowspath] $key_path,
 ) {
   exec { $name:
     command  => template('windows_puppet_certificates/puppet_certs_command.ps1'),
