1ba6b05aef9fd062295a94f7ec6baf9995c67a7d
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-12-11 17:02:15 +0100

    Correct existing cert skip logic

--- templates/puppet_certs_command.ps1
+++ templates/puppet_certs_command.ps1
@@ -257,7 +257,7 @@ switch ($cert_type)
 $cert_store = New-Object -Type System.Security.Cryptography.X509Certificates.X509Store($storename, [System.Security.Cryptography.X509Certificates.StoreLocation]::LocalMachine)
 $cert_store.Open('ReadWrite')
 
-$certs | ForEach {
+ $certs | ForEach {
     if ($key_path -ne '') {
       $pfx_content += [System.IO.File]::ReadAllText($key_path)
     }
@@ -274,13 +274,13 @@ $certs | ForEach {
 
     if ($found) {
       Write-Verbose "Certificate already exists"
-      Continue
+    } else {
+      Write-Verbose "Adding certificate to the store..."
+      $cert_store.Add($pfx) | Out-Null
+
+      Write-Verbose "Certificate added"
     }
 
-    Write-Verbose "Adding certificate to the store..."
-    $cert_store.Add($pfx) | Out-Null
-
-    Write-Verbose "Certificate added"
 }
 
 $cert_store.Close | Out-Null
